517608d4ef3954d19cc9022640f0427ccd6a27e6
Vampirish <vamperish@gmail.com>
2024-09-12 22:58:03 +0200

    Init commit

---
+++ app/src/main/java/com/kehes/studyApplication/LayoutingLessonActivity.kt
@@ -0,0 +1,10 @@
+package com.kehes.studyApplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LayoutingLessonActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/kehes/studyApplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kehes.studyApplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
